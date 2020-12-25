b2df2ebfa2a6860f1e6c61ac007f92cee977a688
Akshat <58493330+JesterRexx-ux@users.noreply.github.com>
2020-12-26 00:11:16 +0530

    Update Some code using mirror.py

--- Context Managers and Else Blocks/Some code using mirror.py
+++ Context Managers and Else Blocks/Some code using mirror.py
@@ -1,4 +1,16 @@
+## Test driving the LookingGLass context managers class
+
 from mirror import LookingGlass
 with LookingGlass() as what:
     print('AKshat','ROnaldo','Batman')
     print(what)
+
+    
+## Exercising LookingGlass without a with block
+
+from mirror import LookingGlass
+manager=LookingGlass()
+manager
+
+monster=manager.__enter__()
+monster=='JABBEREOCKY'
